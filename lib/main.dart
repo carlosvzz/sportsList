51b7bd2cc5f212d2d7c0521778c5e8bd29d5eca9
carlosvzz <carlosvzz@gmail.com>
2019-04-11 00:16:03 -0500

    Tabs y Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './pages/sports.dart';  
+import './pages/sports.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme:_myTheme(),
+      theme: _myTheme(),
       home: MyHomePage(title: 'Sports List'),
     );
   }
@@ -30,6 +30,32 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      drawer: new Drawer(
+          child: new ListView(
+        children: <Widget>[
+         const DrawerHeader(child: Center(child: Text('SPORTS'))),
+          new ListTile(
+            leading: Icon(Icons.screen_lock_portrait),
+            title: new Text('MLB'),
+            onTap: () {},
+          ),
+          new ListTile(
+            leading: Icon(Icons.screen_lock_portrait),
+             title: new Text('NHL'),
+            onTap: () {},
+          ),
+          new ListTile(
+            leading: Icon(Icons.screen_lock_portrait),
+             title: new Text('NBA'),
+            onTap: () {},
+          ),
+          new ListTile(
+            leading: Icon(Icons.screen_lock_portrait),
+             title: new Text('NFL'),
+            onTap: () {},
+          ),
+        ],
+      )),
       body: Sports(),
     );
   }
@@ -37,25 +63,37 @@ class _MyHomePageState extends State<MyHomePage> {
 
 ThemeData _myTheme() {
   return ThemeData(
-    brightness: Brightness.dark,
-    //Texans
-    // primaryColor: Color(0xFF001331),
-    // accentColor: Color(0xFFB82633),
-    //Tigres
-    primaryColor: Color(0xFF005DAB),
-    accentColor: Color(0xFFFCB034),
-    //fontFamily: 'Roboto',
-    buttonColor:  Color(0xFF005DAB),
-    textTheme: TextTheme(
-      headline: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Color(0xFFFCB034)),
-      title: TextStyle(fontSize: 30.0, fontStyle: FontStyle.italic, color: Colors.green),
-      body1: TextStyle(fontSize: 18.0,),
-      body2: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
-      button: TextStyle(fontSize: 17.0,),
-    ),
-    accentTextTheme: TextTheme(
-      body1: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Color(0xFFFCB034)),
-      body2: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.green),
-    )
-  );
+      brightness: Brightness.dark,
+      //Texans
+      // primaryColor: Color(0xFF001331),
+      // accentColor: Color(0xFFB82633),
+      //Tigres
+      primaryColor: Color(0xFF005DAB),
+      accentColor: Color(0xFFFCB034),
+      //fontFamily: 'Roboto',
+      buttonColor: Color(0xFF005DAB),
+      textTheme: TextTheme(
+        headline: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFFFCB034)),
+        title: TextStyle(
+            fontSize: 30.0, fontStyle: FontStyle.italic, color: Colors.green),
+        body1: TextStyle(
+          fontSize: 18.0,
+        ),
+        body2: TextStyle(
+            fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
+        button: TextStyle(
+          fontSize: 17.0,
+        ),
+      ),
+      accentTextTheme: TextTheme(
+        body1: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFFFCB034)),
+        body2: TextStyle(
+            fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.green),
+      ));
 }
--- lib/pages/sports.dart
+++ lib/pages/sports.dart
@@ -9,91 +9,114 @@ class Sports extends StatefulWidget {
   }
 }
 
-class _SportsState extends State<Sports> {
-  String _dropdownValue;
-  DateTime now = DateTime.now();
-  DateTime _selectedDate;
+class _SportsState extends State<Sports> with TickerProviderStateMixin {
+  // String _dropdownValue;
+  // DateTime now = DateTime.now();
+  // DateTime _selectedDate;
 
-  @override
-  void initState() {
-    super.initState();
-     _selectedDate =  DateTime(now.year, now.month, now.day);
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //    _selectedDate =  DateTime(now.year, now.month, now.day);
+  // }
 
+  // Future<Null> _selectDate(BuildContext context) async {
+  //   DateTime picked = await showDatePicker(
+  //       context: context,
+  //       initialDate: _selectedDate,
+  //       firstDate: new DateTime.now().subtract(Duration(days: 3)),
+  //       lastDate: new DateTime.now().add(Duration(days: 3)));
+  //   if (picked != null && picked != _selectedDate)
+  //     setState(() => _selectedDate = picked);
+  // }
 
-  Future<Null> _selectDate(BuildContext context) async {
-    DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: _selectedDate,
-        firstDate: new DateTime.now().subtract(Duration(days: 3)),
-        lastDate: new DateTime.now().add(Duration(days: 3)));
-    if (picked != null && picked != _selectedDate)
-      setState(() => _selectedDate = picked);
-  }
+  // Widget rowFilters(BuildContext context) {
+  //   return Row(
+  //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //     children: <Widget>[
+  //       DropdownButton<String>(
+  //         style: Theme.of(context).textTheme.body2,
+  //         value: _dropdownValue,
+  //         hint: Text('<Seleccionar>'),
+  //         onChanged: (String newValue) {
+  //           setState(() {
+  //             _dropdownValue = newValue;
+  //           });
+  //         },
+  //         items: <String>['NBA', 'NHL', 'MLB', 'NFL']
+  //             .map<DropdownMenuItem<String>>((String value) {
+  //           return DropdownMenuItem<String>(
+  //             value: value,
+  //             child: Text(value),
+  //           );
+  //         }).toList(),
+  //       ),
+  //       RaisedButton(
+  //         //elevation: 0.0,
+  //         onPressed: () => _selectDate(context),
+  //         color: Theme.of(context).accentColor,
+  //         child: Text(
+  //           _formatoFecha(),
+  //           style: Theme.of(context).textTheme.button,
+  //         ),
+  //         shape: RoundedRectangleBorder(
+  //           borderRadius: new BorderRadius.circular(17.0),
+  //         ),
+  //       ),
+  //     ],
+  //   );
+  // }
 
-  Widget rowFilters(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        DropdownButton<String>(          
-          style: Theme.of(context).textTheme.body2,
-          value: _dropdownValue,
-          hint: Text('<Seleccionar>'),
-          onChanged: (String newValue) {
-            setState(() {
-              _dropdownValue = newValue;
-            });
-          },
-          items: <String>['NBA', 'NHL', 'MLB', 'NFL']
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-        ),
-        RaisedButton(
-          //elevation: 0.0,
-          onPressed: () => _selectDate(context),
-          color: Theme.of(context).accentColor,
-          child: Text(
-            _formatoFecha(),
-            style: Theme.of(context).textTheme.button,
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(17.0),
-          ),
-        ),
-      ],
-    );
-  }
+  // String _formatoFecha() {
+  //    DateTime hoy = DateTime(now.year, now.month, now.day);
+  //    DateTime ayer = DateTime(now.year, now.month, now.day-1);
+  //    DateTime manana = DateTime(now.year, now.month, now.day+1);
 
-  String _formatoFecha() {
-     DateTime hoy = DateTime(now.year, now.month, now.day);
-     DateTime ayer = DateTime(now.year, now.month, now.day-1);
-     DateTime manana = DateTime(now.year, now.month, now.day+1);
+  //   if (_selectedDate == hoy) {
+  //     return 'HOY';
+  //   } else if (_selectedDate == manana) {
+  //     return 'MAÑANA';
+  //   } else if (_selectedDate == ayer) {
+  //     return 'AYER';
+  //   } else {
+  //     return formatDate(_selectedDate, [yyyy, '-', mm, '-', dd]);
+  //   }
+  // }
 
-    if (_selectedDate == hoy) {
-      return 'HOY';
-    } else if (_selectedDate == manana) {
-      return 'MAÑANA';
-    } else if (_selectedDate == ayer) {
-      return 'AYER';
-    } else {
-      return formatDate(_selectedDate, [yyyy, '-', mm, '-', dd]);
-    }
-  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Column(
+  //     children: <Widget>[
+  //       rowFilters(context),
+  //       Divider(),
+  //       Expanded(
+  //         child: ListGames(),
+  //       )
+  //     ],
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        rowFilters(context),
-        Divider(),
-        Expanded(
-          child: ListGames(),
-        )
-      ],
+    TabController tabctl = new TabController(length: 7, vsync: this);
+    double width = MediaQuery.of(context).size.width;
+    double yourWidth = width  /10;
+
+    return TabBar(
+      controller: tabctl,
+      indicatorColor: Colors.red,
+      indicatorWeight: 2.0,
+      isScrollable: true,
+      labelPadding: EdgeInsets.only(left: yourWidth, right: yourWidth),
+      tabs: <Widget>[
+        Tab(icon: Icon(Icons.calendar_today), text: "lun, 08/04",),
+        Tab(icon: Icon(Icons.calendar_today, color: Theme.of(context).primaryColor), text: "AYER",),
+        Tab(icon: Icon(Icons.today, color: Theme.of(context).accentColor), text: "HOY",),
+        Tab(icon: Icon(Icons.date_range, color: Theme.of(context).primaryColor), text: "MANANA",),
+        Tab(icon: Icon(Icons.calendar_today), text: "vie, 12/04",),
+        Tab(icon: Icon(Icons.calendar_today), text: "sab, 13/04",),
+        Tab(icon: Icon(Icons.calendar_today), text: "dom, 14/04",),
+       ],
     );
   }
 }
